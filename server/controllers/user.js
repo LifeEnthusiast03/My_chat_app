b26b484496b08f1440a83bb07346725fd28175ce
SOUGATA SAHA <sahasougata820@gmail.com>
2025-06-27 00:15:41 +0530

    modify the controllers

---
+++ server/controllers/user.js
@@ -0,0 +1,38 @@
+import User from '../models/user.js'
+
+async function createUser(req,res) {
+    try{
+    const {username,email,password}=req.body;
+    const user = new User({
+        username,
+        email,
+        password
+    });
+    await user.save();
+    res.status(201).json({
+        message:'user created successfully',
+        user:user.toJSON()
+    });
+    }
+    catch(error){
+        res.status(400).json({
+            message:error.message,
+            deatils:errors.errors
+        });
+    }
+}
+async function getUser(req,res) {
+    try{
+        const users= await User.find({}).select('-password');
+        res.json({
+            count:users.length,
+            users
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            error:error.message
+        });
+    }
+}
+export {createUser,getUser};
--- server/routes/test.js
+++ server/routes/test.js
@@ -1,46 +1,10 @@
 import express from  'express'
-import User from '../models/user.js'
+import {createUser,getUser} from '../controllers/user.js';
 
 const router = express.Router();
 
-router.post('/create-user',async(req,res)=>{
-    try{
-    const {username,email,password}=req.body;
-    const user = new User({
-        username,
-        email,
-        password
-    });
-    await user.save();
-    res.status(201).json({
-        message:'user created successfully',
-        user:user.toJSON()
-    });
-    }
-    catch(error){
-        res.status(400).json({
-            message:error.message,
-            deatils:errors.errors
-        });
-    }
-
-});
-
-
-router.get('/users',async(req,res)=>{
-    try{
-        const users= await User.find({}).select('-password');
-        res.json({
-            count:users.length,
-            users
-        });
-    }
-    catch(error){
-        res.status(500).json({
-            error:error.message
-        });
-    }
-});
+router.post('/create-user',createUser);
+router.get('/users',getUser);
 
 
 export default router
